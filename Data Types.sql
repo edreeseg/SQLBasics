2cf92c908c916a80763ebd4ff259370c7add9578
edreeseg <edward.reeseg@gmail.com>
2018-08-30 20:00:37 -0400

    Data types and tables

---
+++ Data Types.sql
@@ -0,0 +1,8 @@
+/* Datatypes
+    INT - Whole numbers.
+    DECIMAL(M,N) - Decimal Numbers - Exact Value. M = Total # of digits, N = Total # of digits after decimal point.
+    VARCHAR(1) - String of text of length 1.
+    BLOB - Binary Large Object - Stores large data.
+    DATE - "YYYY-MM-DD"
+    TIMESTAMP - "YYYY-MM-DD HH:MM:SS" Used for recording time.
+*/
